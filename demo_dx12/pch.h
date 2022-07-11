558d92d0af9b566d132dd9b62dc8504e7121bde5
wiwiwuwuwa <95725407+wiwiwuwuwa@users.noreply.github.com>
2022-07-11 05:12:40 +0300

    added tinyutf8 library

--- demo_dx12/pch.h
+++ demo_dx12/pch.h
@@ -1,10 +1,15 @@
 ﻿#pragma once
 
+#ifndef NOMINMAX
+	#define NOMINMAX
+#endif
+
 #include <Unknwn.h>
 #include <winrt/Windows.ApplicationModel.Core.h>
 #include <winrt/Windows.UI.Core.h>
 #include <d3d12.h>
 #include <dxgi1_6.h>
+#include <tinyutf8/tinyutf8.h>
 
 using namespace winrt::Windows::ApplicationModel::Core;
 using namespace winrt::Windows::UI::Core;

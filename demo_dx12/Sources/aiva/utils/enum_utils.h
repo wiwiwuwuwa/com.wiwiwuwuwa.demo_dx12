1d315b2ef6f5a377de1103889b06c0f2e2ad1381
wiwiwuwuwa <95725407+wiwiwuwuwa@users.noreply.github.com>
2022-07-17 15:45:43 +0300

    added some functions to enum utils struct

--- demo_dx12/Sources/aiva/utils/enum_utils.h
+++ demo_dx12/Sources/aiva/utils/enum_utils.h
@@ -10,11 +10,28 @@ namespace aiva::utils
 
 	public:
 		template <typename TEnum>
+		static TEnum And(TEnum const a, TEnum const b);
+
+		template <typename TEnum>
 		static TEnum Or(TEnum const a, TEnum const b);
+
+		template <typename TEnum>
+		static bool HasFlag(TEnum const a, TEnum const flag);
 	};
 }
 
 // --------------------------------------------------------
+
+template <typename TEnum>
+TEnum aiva::utils::EnumUtils::And(TEnum const a, TEnum const b)
+{
+	using NumberType = std::underlying_type_t<TEnum>;
+
+	auto const resultNumber = static_cast<NumberType>(a) & static_cast<NumberType>(b);
+	auto const resultEnum = static_cast<TEnum>(resultNumber);
+
+	return resultEnum;
+}
 
 template <typename TEnum>
 TEnum aiva::utils::EnumUtils::Or(TEnum const a, TEnum const b)
@@ -25,4 +42,10 @@ TEnum aiva::utils::EnumUtils::Or(TEnum const a, TEnum const b)
 	auto const resultEnum = static_cast<TEnum>(resultNumber);
 
 	return resultEnum;
+}
+
+template <typename TEnum>
+bool aiva::utils::EnumUtils::HasFlag(TEnum const a, TEnum const flag)
+{
+	return And(a, flag) == flag;
 }

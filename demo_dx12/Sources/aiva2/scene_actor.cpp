fba60bde0f7cb5c84d4cbebcaf1871eb083780bf
wiwiwuwuwa <wiwiwuwuwa@gmail.com>
2023-04-22 18:33:48 +0200

    added api for components creation

--- demo_dx12/Sources/aiva2/scene_actor.cpp
+++ demo_dx12/Sources/aiva2/scene_actor.cpp
@@ -1,7 +1,9 @@
 #include <pch.h>
 #include <aiva2/scene_actor.hpp>
 
+#include <aiva2/assert.hpp>
 #include <aiva2/engine.hpp>
+#include <aiva2/scene_component.hpp>
 #include <aiva2/scene_graph.hpp>
 
 namespace aiva2
@@ -25,5 +27,34 @@ namespace aiva2
     auto scene_actor_t::get_engine() const -> engine_t&
     {
         return get_graph().get_engine();
+    }
+
+    auto scene_actor_t::get_component_ptr(size_t const index) -> std::shared_ptr<scene_component_t>
+    {
+        assert_t::check_bool(index >= decltype(index){}, "(index) is out of range");
+        assert_t::check_bool(index < std::size(m_components), "(index) is out of range");
+
+        return m_components[index];
+    }
+
+    auto scene_actor_t::get_component_ref(size_t const index) -> scene_component_t&
+    {
+        auto const component = get_component_ptr(index);
+        assert_t::check_bool(component, "(component) is not valid");
+
+        return (*component);
+    }
+
+    auto scene_actor_t::num_component() const -> size_t
+    {
+        return std::size(m_components);
+    }
+
+    auto scene_actor_t::rem_component(size_t const index) -> void
+    {
+        assert_t::check_bool(index >= decltype(index){}, "(index) is out of range");
+        assert_t::check_bool(index < std::size(m_components), "(index) is out of range");
+
+        m_components.erase(std::next(std::begin(m_components), index));
     }
 }
--- demo_dx12/Sources/aiva2/scene_actor.hpp
+++ demo_dx12/Sources/aiva2/scene_actor.hpp
@@ -29,5 +29,108 @@ namespace aiva2
         scene_graph_t& m_graph;
 
         // ------------------------------------------------
+
+    public:
+        template <typename t_cmp>
+        auto add_component_ptr() -> std::shared_ptr<t_cmp>;
+
+        template <typename t_cmp>
+        auto add_component_ref() -> t_cmp&;
+
+        template <typename t_cmp>
+        auto get_component_ptr() -> std::shared_ptr<t_cmp>;
+
+        template <typename t_cmp>
+        auto get_component_ref() -> t_cmp&;
+
+        template <typename t_cmp>
+        auto has_component() -> bool;
+
+        template <typename t_cmp>
+        auto rem_component() -> void;
+
+    public:
+        auto get_component_ptr(size_t const index) -> std::shared_ptr<scene_component_t>;
+
+        auto get_component_ref(size_t const index) -> scene_component_t&;
+
+        auto num_component() const -> size_t;
+
+        auto rem_component(size_t const index) -> void;
+
+    private:
+        std::vector<std::shared_ptr<scene_component_t>> m_components{};
+
+        // ------------------------------------------------
     };
+}
+
+// --------------------------------------------------------
+
+namespace aiva2
+{
+    template <typename t_cmp>
+    auto scene_actor_t::add_component_ptr() -> std::shared_ptr<t_cmp>
+    {
+        auto const component = std::make_shared<t_cmp>(*this);
+        assert_t::check_bool(component, "(component) is not valid");
+
+        return m_components.emplace_back(component);
+    }
+
+    template <typename t_cmp>
+    auto scene_actor_t::add_component_ref() -> t_cmp&
+    {
+        auto const component = add_component_ptr<t_cmp>();
+        assert_t::check_bool(component, "(component) is not valid");
+
+        return (*component);
+    }
+
+    template <typename t_cmp>
+    auto scene_actor_t::get_component_ptr() -> std::shared_ptr<t_cmp>
+    {
+        for (auto i = size_t{}; i < num_component(); i++)
+        {
+            auto const component = get_component_ptr(i);
+            assert_t::check_bool(component, "(component) is not valid");
+
+            if (auto const cmp = std::dynamic_pointer_cast<t_cmp>(component))
+            {
+                return cmp;
+            }
+        }
+
+        return {};
+    }
+
+    template <typename t_cmp>
+    auto scene_actor_t::get_component_ref() -> t_cmp&
+    {
+        auto const component = get_component_ptr<t_cmp>();
+        assert_t::check_bool(component, "(component) is not valid");
+
+        return (*component);
+    }
+
+    template <typename t_cmp>
+    auto scene_actor_t::has_component() -> bool
+    {
+        return (get_component_ptr<t_cmp>() != nullptr);
+    }
+
+    template <typename t_cmp>
+    auto scene_actor_t::rem_component() -> void
+    {
+        for (auto i = num_component() - 1; i >= 0; i--)
+        {
+            auto const component = get_component_ptr(i);
+            assert_t::check_bool(component, "(component) is not valid");
+
+            if (std::dynamic_pointer_cast<t_cmp>(component))
+            {
+                rem_component(i);
+            }
+        }
+    }
 }
